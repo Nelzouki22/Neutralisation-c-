9b10a420504f78fd94250dc444ec3a6d673fb100
NADIR ELZOUKI <elzoukigroup2018@gmail.com>
2024-10-03 21:28:07 -0400

    Add project files.

---
+++ Neutralisation c++/Source.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <string>
+
+std::string neutralise(const std::string& s1, const std::string& s2) {
+    std::string result;
+    for (size_t i = 0; i < s1.length(); ++i) {
+        if (s1[i] == '+' && s2[i] == '+') {
+            result += '+';
+        }
+        else if (s1[i] == '-' && s2[i] == '-') {
+            result += '-';
+        }
+        else {
+            result += '0';
+        }
+    }
+    return result;
+}
+
+int main() {
+    // Example usage
+    std::cout << neutralise("+-+", "+--") << " ➞ " << "+-0" << std::endl;
+    std::cout << neutralise("--++--", "++--++") << " ➞ " << "000000" << std::endl;
+    std::cout << neutralise("-+-+-+", "-+-+-+") << " ➞ " << "-+-+-+" << std::endl;
+    std::cout << neutralise("-++-", "-+-+") << " ➞ " << "-+00" << std::endl;
+
+    return 0;
+}
